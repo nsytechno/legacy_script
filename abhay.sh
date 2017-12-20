7022249d74ddc5fa60d4abf6aa9e50ae6d4b5a19
Nikhil yadav <nsytechno@gmail.com>
2017-12-20 21:46:01 +0530

    Add ccache

--- abhay.sh
+++ abhay.sh
@@ -4,6 +4,10 @@
 # cd rom_repo
 # curl https://raw.githubusercontent.com/redking12/legacy_script/master/abhay.sh > script_build.sh
 
+echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
+export USE_CCACHE=1
+export PATH="/usr/lib/ccache/:$PATH"
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
 
 # Build ROM
 . build/envsetup.sh
@@ -11,3 +15,4 @@ lunch validus_kuntao-userdebug
 export KBUILD_BUILD_USER=Abhay
 export KBUILD_BUILD_HOST=PoloniumBox
 brunch kuntao
+
