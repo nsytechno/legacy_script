0a98e73b0e1fad023952680798eab647099e45ea
Abhay Ruparel <abhayruparel2000@gmail.com>
2017-12-20 21:41:17 +0530

    edit script for making validus

--- abhay.sh
+++ abhay.sh
@@ -4,49 +4,10 @@
 # cd rom_repo
 # curl https://raw.githubusercontent.com/redking12/legacy_script/master/abhay.sh > script_build.sh
 
-# Some User's Details. Please fill it with your own details.
-export KBUILD_BUILD_USER=Abhay
-export KBUILD_BUILD_HOST=PoloniumBox
-
-# Replace "legacy" with your own SSH Username in lowercase
-username=surajparmar6
-
-# Colors makes things beautiful
-export TERM=xterm
-
-    red=$(tput setaf 1)             #  red
-    grn=$(tput setaf 2)             #  green
-    blu=$(tput setaf 4)             #  blue
-    cya=$(tput setaf 6)             #  cyan
-    txtrst=$(tput sgr0)             #  Reset
-
-# CCACHE UMMM!!! Cooks my builds fast
-
-if [ "$use_ccache" = "yes" ];
-then
-echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
-export USE_CCACHE=1
-export CCACHE_DIR=/home/ccache/surajparmar6
-prebuilts/misc/linux-x86/ccache/ccache -M 50G
-fi
-
-if [ "$use_ccache" = "clean" ];
-then
-export CCACHE_DIR=/home/ccache/surajparmar6
-ccache -C
-wait
-echo -e ${grn}"CCACHE Cleared"${txtrst};
-fi
-
-# Its Clean Time
-if [ "$make_clean" = "yes" ];
-then
-make clean && make clobber
-wait
-echo -e ${cya}"OUT dir from your repo deleted"${txtrst};
-fi
 
 # Build ROM
 . build/envsetup.sh
-lunch "$lunch_command"_"$device"-userdebug
-make "$target_command" -j16
+lunch validus_kuntao-userdebug
+export KBUILD_BUILD_USER=Abhay
+export KBUILD_BUILD_HOST=PoloniumBox
+brunch kuntao
